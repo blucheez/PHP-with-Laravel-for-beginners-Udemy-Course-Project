4c944ed58d018568e92b51a01b38c8eb6bf0787c
blucheez <petreistvan94@gmail.com>
2023-01-20 15:13:12 +0100

    controllers fundamentals

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
 
@@ -33,4 +33,8 @@ Route::get('admin/posts/example', array('as'=>'admin.home',  function () {
     $url = route('admin.home');
 
     return "this url is" . $url;
-}));
+})); */
+
+/* Route::get('/post/{id}/{name}', 'PostsController@index'); */
+
+Route::resource('posts', 'PostsController');
